4aa8cbd90e7885c24530084bbafb8f5b43b843a5
Ross Wilson <ross.wilson@homeflow.co.uk>
2024-05-04 13:20:17 +0100

    Add handlers and scopes for new requests to begin mapping out user flow

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod db;
 
-use actix_web::{web::{self, Json}, App, HttpServer, Responder, HttpResponse, error};
+use actix_web::{
+    web::{self},
+    App, HttpResponse, HttpServer, Responder,
+};
 use db::users::{init_users, User, UserError};
 use env_logger::Env;
 use std::collections::HashMap;
@@ -10,13 +13,21 @@ type Users = HashMap<String, User>;
 async fn login(data: web::Data<Users>) -> Result<impl Responder, UserError> {
     println!("{:?}", data);
     match data.get("3") {
-        Some(user) => {
-            Ok(HttpResponse::Ok().json(user))
-        }
-        None => {
-            Err(UserError::UserNotFound)
-        }
+        Some(user) => Ok(HttpResponse::Ok().json(user)),
+        None => Err(UserError::UserNotFound),
     }
+}
+
+async fn verify() -> impl Responder {
+    HttpResponse::Ok().body("Verify token")
+}
+
+async fn update_admin_user() -> impl Responder {
+    HttpResponse::Ok().json("Update admin user")
+}
+
+async fn update_site_user() -> impl Responder {
+    HttpResponse::Ok().json("Update site user")
 }
 
 #[actix_web::main]
@@ -26,7 +37,13 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .app_data(web::Data::new(init_users()))
-            .service(web::scope("/auth").route("/login", web::get().to(login)))
+            .service(
+                web::scope("/auth")
+                    .route("/login", web::get().to(login))
+                    .route("/verify", web::get().to(verify)),
+            )
+            .service(web::scope("/admin").route("/update", web::patch().to(update_admin_user)))
+            .service(web::scope("/site").route("/update", web::patch().to(update_site_user)))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
